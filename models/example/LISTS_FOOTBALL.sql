725f312ac04bbb82ee1f2bbaa6d967db4de80b5a
Bharath Reddy <89402199+Bharath-63@users.noreply.github.com>
2022-03-30 13:23:07 +0530

    Update LISTS_FOOTBALL.sql

--- models/example/LISTS_FOOTBALL.sql
+++ models/example/LISTS_FOOTBALL.sql
@@ -1,4 +1,4 @@
-create or update  table "DB".PUBLIC."LISTS_FOOTBALL"  as
+create or update  table "DB"."PUBLIC"."LISTS_FOOTBALL"  as
 (select * from(
 
 with __dbt__cte__LISTS_FOOTBALL_AB1 as (
@@ -17,7 +17,7 @@ select
     _AIRBYTE_AB_ID,
     _AIRBYTE_EMITTED_AT,
     convert_timezone('UTC', current_timestamp()) as _AIRBYTE_NORMALIZED_AT
-from "DB".PUBLIC."LISTS" as table_alias
+from "DB".PUBLIC._AIRBYTE_RAW_LISTS as table_alias
 -- FOOTBALL at lists/football
 cross join table(flatten(FOOTBALL)) as FOOTBALL
 where 1 = 1
